9aa74a3772f9d1470a5afd4ad12b4934fac28ee4
madxmike <mep053@gmail.com>
2026-03-09 23:07:22 -0400

    Add GitHub icon to footer using FontAwesome

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,7 @@
+
+export default new Map([
+["src/content/blog/getting-started-with-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgetting-started-with-astro.mdx&astroContentModuleFlag=true")],
+["src/content/blog/draft-future-ideas.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdraft-future-ideas.mdx&astroContentModuleFlag=true")],
+["src/content/blog/typography-showcase.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftypography-showcase.mdx&astroContentModuleFlag=true")],
+["src/content/blog/building-interactive-components.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbuilding-interactive-components.mdx&astroContentModuleFlag=true")]]);
+		
